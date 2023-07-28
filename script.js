62cda88e0441fbd60452f63c172ee02386acce0f
Charvit Zalavadiya <120319997+CharvitZalavadiyaOfficial@users.noreply.github.com>
2023-07-28 09:36:30 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,96 @@
+let a = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=Fru4OP8OtiI&ab_channel=TaarakMehtaKaOoltahChashmah'
+}
+img1.addEventListener('click', a)
+let b = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=hy40PC2bfOw&ab_channel=NavritGaming'
+}
+img2.addEventListener('click', b)
+let c = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=mdvMXDUoWto&ab_channel=NOTYOURTYPE'
+}
+img3.addEventListener('click', c)
+let d = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=DhZeTWi76A8&ab_channel=TechnoGamerz'
+}
+img4.addEventListener('click', d)
+let e = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=dH-7gDtcKpk&ab_channel=CarryisLive'
+}
+img5.addEventListener('click', e)
+let f = function (e) {
+    location.href = 'hhttps://www.youtube.com/watch?v=ama3qvvwDx0&ab_channel=TechBurner'
+}
+img6.addEventListener('click', f)
+let g = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=yurwWqUGYiM&ab_channel=MR.INDIANHACKER'
+}
+img7.addEventListener('click', g)
+let h = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=PPI67dtu6dM&ab_channel=SonySAB'
+}
+img8.addEventListener('click', h)
+let i = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=AOcdgJmfwXg&ab_channel=Techno'
+}
+img9.addEventListener('click', i)
+let j = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=0VwpEtRQWOE&ab_channel=NavritGamingExtraa'
+}
+img10.addEventListener('click', j)
+let k = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=-tj1eCR3x-M&ab_channel=TechBurner'
+}
+img11.addEventListener('click', k)
+let l = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=D932kFjf184&ab_channel=AnshuBisht'
+}
+img12.addEventListener('click', l)
+let m = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=5vIAWGHCtHw&ab_channel=TechBurner'
+}
+img13.addEventListener('click', m)
+let n = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=8xHxXDogVR8&ab_channel=AnshuBisht'
+}
+img14.addEventListener('click', n)
+let o = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=AP3_V7KXHs4&ab_channel=CodeWithHarry'
+}
+img15.addEventListener('click', o)
+let p = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=VJW4GNsWTjo&ab_channel=Infinitylo-fi'
+}
+img16.addEventListener('click', p)
+let q = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=r-ozeuKLZAY&ab_channel=OggyHindi-%E0%A4%B9%E0%A4%BF%E0%A4%A8%E0%A5%8D%E0%A4%A6%E0%A5%80'
+}
+img17.addEventListener('click', q)
+let r = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=iF48V8Q_4MA&ab_channel=TechnoGamerz'
+}
+img18.addEventListener('click', r)
+let s = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=fzy3uXy2T9s&ab_channel=DynamoGaming'
+}
+img19.addEventListener('click', s)
+let t = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=GTjN0SxdK5w&ab_channel=CodeWithHarry'
+}
+img20.addEventListener('click', t)
+let u = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=WuOWEc1aBsY&ab_channel=BBGAMING'
+}
+img21.addEventListener('click', u)
+let v = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=CLulkg5zKeY&ab_channel=SonySAB'
+}
+img22.addEventListener('click', v)
+let w = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=Zi3-moBdvfw&ab_channel=Techlogy'
+}
+img23.addEventListener('click', w)
+let x = function (e) {
+    location.href = 'https://www.youtube.com/watch?v=oFnIe-RpkE4&ab_channel=CodeWithHarry'
+}
+img24.addEventListener('click', x)
